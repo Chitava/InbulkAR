ccea04b00553802b95490a48d4176fc64c5eb69e
Chitava <112967237+Chitava@users.noreply.github.com>
2023-10-02 18:46:57 +0300

    Добавил обработку ошибки в данных по зарплате (отсутствует либо ставка либо стоимость переработки)

--- controller.py
+++ controller.py
@@ -119,43 +119,45 @@ def Create_salary_in_one_month(date1, date2, month):
     data = db.Select_work_days_join_workers(month)
     ld=len(data)
     for item in data:
-        name = str(item[0])
-        work_time = datetime.datetime(2000, 1, 1, 9, 0)
-        work_day = 0
-        salary = 0
-        elabor_time = datetime.datetime(2000, 1, 1, 0, 0)
-        elabor_salary = 0
-        all_elab_time = datetime.datetime(2000, 1, 1, 0, 0)
-        if (item[1] == ""):
-            break
-        else:
-            wage = item[1]
-            elab = item[2]
-
-            houre_wage = int(wage) / 8
-            result = []
-            for i in range(3+date1, 4+date2):
-                temp = item[i].split('.')
-                if len(temp) > 1:
-                    curent_time = datetime.datetime(2000, 1, 1, int(temp[0]), int(temp[1]))
-                    if curent_time.hour < work_time.hour:
-                        day_wage = (float(item[i]) - 1) * houre_wage
-                        salary += day_wage
-                        work_day += 1
-                    else:
-                        salary += wage
-                        elab_time = curent_time - work_time
-                        all_elab_time += elab_time
-                        work_day += 1
-        temp_elab_time = (all_elab_time.day - 1) * 24
-        month_salary = salary + (float(f"{temp_elab_time + all_elab_time.hour}.{all_elab_time.minute}") * elab)
-        result.append(work_day)  # Кол-во отработаных дней
-        result.append(f"{temp_elab_time + all_elab_time.hour},{all_elab_time.minute}")  # Сумма часов переработки
-        result.append(round(salary, 2))  # Зарплата за отработанные дни
-        result.append(
-            float(f"{temp_elab_time + all_elab_time.hour}.{all_elab_time.minute}") * elab)  # Зарплата за переработку
-        result.append(round(month_salary, 2))  # Зарплата за месяц
-        salary_all[name] = result
+        try:
+            name = str(item[0])
+            work_time = datetime.datetime(2000, 1, 1, 9, 0)
+            work_day = 0
+            salary = 0
+            elabor_time = datetime.datetime(2000, 1, 1, 0, 0)
+            elabor_salary = 0
+            all_elab_time = datetime.datetime(2000, 1, 1, 0, 0)
+            if (item[1] == ""):
+                break
+            else:
+                wage = item[1]
+                elab = item[2]
+                houre_wage = int(wage) / 8
+                result = []
+                for i in range(3+date1, 4+date2):
+                    temp = item[i].split('.')
+                    if len(temp) > 1:
+                        curent_time = datetime.datetime(2000, 1, 1, int(temp[0]), int(temp[1]))
+                        if curent_time.hour < work_time.hour:
+                            day_wage = (float(item[i]) - 1) * houre_wage
+                            salary += day_wage
+                            work_day += 1
+                        else:
+                            salary += wage
+                            elab_time = curent_time - work_time
+                            all_elab_time += elab_time
+                            work_day += 1
+                        temp_elab_time = (all_elab_time.day - 1) * 24
+                        month_salary = salary + (float(f"{temp_elab_time + all_elab_time.hour}.{all_elab_time.minute}") * elab)
+                        result.append(work_day)  # Кол-во отработаных дней
+                        result.append(f"{temp_elab_time + all_elab_time.hour},{all_elab_time.minute}")  # Сумма часов переработки
+                        result.append(round(salary, 2))  # Зарплата за отработанные дни
+                        result.append(
+                        float(f"{temp_elab_time + all_elab_time.hour}.{all_elab_time.minute}") * elab)  # Зарплата за переработку
+                        result.append(round(month_salary, 2))  # Зарплата за месяц
+                        salary_all[name] = result
+        except Exception:
+            view.Messagebox("Внимание", f'"Ошибка в данных по зарплате у {name}"')
     return salary_all
 
 
